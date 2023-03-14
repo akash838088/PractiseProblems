678e90923a5832d8c270f629a068fdc30dc50432
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-03-14 19:04:43 +0530

    Add UC1-EvenOrOdd

---
+++ PractiseProblems/PractiseProblems/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Logical Programing");
+            bool flag = false;
+            while (!flag)
+            {
+                Console.WriteLine("1:Check Odd Even Number Program\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        OddOrEvenNumber.EvenOddNumbers();
+                        break;
+                    default:
+                        Console.WriteLine("Choose a valid Options");
+                        break;
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
